e90b32e76811b8bf54aa329a77a2dc1403267669
Raiza2309 <praditaramadhan00@gmail.com>
2020-07-27 09:19:10 +0700

    Belajar Website

---
+++ assets/js/nav.js
@@ -0,0 +1,10 @@
+let nav = document.write(
+    `<nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="install-xampp.html">Install</a></li>
+            <li><a href="membuat-database.html">Membuat Database</a></li>
+            <li><a href="menambahkan-data.html">Menambahkan Data</a></li>
+        </ul>
+    </nav>`
+);
